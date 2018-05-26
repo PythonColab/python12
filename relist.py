eb17f4cfdf4dfa3e445c7cffe6d1509c779a8179
suryadutt007 <39251001+suryadutt007@users.noreply.github.com>
2018-05-26 12:42:55 +0530

    Add files via upload

---
+++ relist.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 26 09:49:25 2018
+
+@author: SURAJ
+"""
+
+'''l1 = [2,45,35,"surya"]
+a=input("enter your num")
+l1.append(a)
+print (l1)
+l2=[234,23.2,34]
+print (len(l2))
+print (l1+l2)
+print (45 in l1)
+print (l1[1])
+print (l2.sort())'''
+
+
+
+
+# tuple
+'''t1 = (12,343,2232,"surya")
+t2= (12,545,545,12233,5678)
+print (t1)
+print (t1[3])
+print (len(t1))
+print (t1+t2)'''
+
+
+
+#dictionary
+'''d1 ={"name":"surya","age":23}
+a=input("enter your num")
+d1["rel"]= a
+print (d1)'''
+
+
+
+#looping
+'''n = 5
+for i in range(0, n):
+     
+        for j in range(0, i+1):
+         
+        
+            print("* ",end="")
+      
+        
+        print("\n")
+'''
+
+
+#loop with if/else
+'''def star(n):
+  for i in range(0, n):
+     
+        for j in range(0, i+1):
+            print("* ",end="") 
+            if j%2==0:   
+                print("# ",end="")
+      
+        
+        print("\n")
+        
+        
+        
+
+         
+n=int(input("enter limit"))
+star (n);
+'''
+
+#loopin in list
+'''a=[]
+n= int(input("Enter the number of elements in list:"))
+for x in range(0,n):
+    e=int(input("Enter element"))
+    a.append(e)
+
+b= []
+for x in a:
+    if x not in b:
+        b.append(x)
+        
+print("Non-duplicate items:")
+print(b)'''
+
+# functions
+'''def go(a,b):
+   print (a+b)
+
+l1=[1,2,3,4] 
+l2=[34,54,656,67]
+go (l1,l2);''' 
+
+
+
